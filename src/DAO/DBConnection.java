721738039ad7fdbc7b685fd67d994863e856991f
bjostad <13590108+bjostad@users.noreply.github.com>
2022-03-25 00:10:57 -0700

    beginnings of sql connection

---
+++ src/DAO/DBConnection.java
@@ -0,0 +1,34 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String databaseName="client_schedule";
+    private static final String DB_URL="jdbc:mysql://localhost:3306/"+databaseName;
+    private static final String username="sqlUser";
+    private static final String password="Passw0rd!";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    static Connection conn;
+
+    /**
+     * Connect to mySQL database
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws Exception
+     */
+    public static void makeConnection() throws ClassNotFoundException, SQLException, Exception {
+        conn=(Connection) DriverManager.getConnection(DB_URL,username,password);
+    }
+
+    /**
+     * close mySQL database connection
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws Exception
+     */
+    public static void closeConnection() throws ClassNotFoundException, SQLException, Exception{
+        conn.close();
+    }
+}
